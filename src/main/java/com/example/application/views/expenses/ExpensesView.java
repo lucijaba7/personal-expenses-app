696cf9d01ec9453398c4d6b71a21b727a2d1ccf9
start.vaadin.com <no-reply@vaadin.com>
2022-01-28 20:53:48 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/expenses/ExpensesView.java
@@ -0,0 +1,41 @@
+package com.example.application.views.expenses;
+
+import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.HasComponents;
+import com.vaadin.flow.component.HasStyle;
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.littemplate.LitTemplate;
+import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.component.template.Id;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@PageTitle("Expenses")
+@Route(value = "expenses", layout = MainLayout.class)
+@Tag("expenses-view")
+@JsModule("./views/expenses/expenses-view.ts")
+public class ExpensesView extends LitTemplate implements HasComponents, HasStyle {
+
+    @Id
+    private Select<String> sortBy;
+
+    public ExpensesView() {
+        addClassNames("expenses-view", "flex", "flex-col", "h-full");
+        sortBy.setItems("Popularity", "Newest first", "Oldest first");
+        sortBy.setValue("Popularity");
+
+        add(new ExpensesViewCard("Snow mountains under stars",
+                "https://images.unsplash.com/photo-1519681393784-d120267933ba?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"));
+        add(new ExpensesViewCard("Snow covered mountain",
+                "https://images.unsplash.com/photo-1512273222628-4daea6e55abb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"));
+        add(new ExpensesViewCard("River between mountains",
+                "https://images.unsplash.com/photo-1536048810607-3dc7f86981cb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=375&q=80"));
+        add(new ExpensesViewCard("Milky way on mountains",
+                "https://images.unsplash.com/photo-1515705576963-95cad62945b6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80"));
+        add(new ExpensesViewCard("Mountain with fog",
+                "https://images.unsplash.com/photo-1513147122760-ad1d5bf68cdb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"));
+        add(new ExpensesViewCard("Mountain at night",
+                "https://images.unsplash.com/photo-1562832135-14a35d25edef?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=815&q=80"));
+    }
+}
